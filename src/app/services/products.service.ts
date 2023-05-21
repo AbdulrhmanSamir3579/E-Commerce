d88b46873c84722ab6cd138bfd43d6c4b7d264cf
mythic357 <101456209+mythic357@users.noreply.github.com>
2023-05-21 13:40:50 +0300

    update api base url

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,28 +1,23 @@
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class ProductsService {
+  baseUrl: string = "https://ecommerce.routemisr.com/";
   constructor(private _HttpClient: HttpClient) {}
 
   getProducts(): Observable<any> {
-    return this._HttpClient.get(
-      'https://route-ecommerce.onrender.com/api/v1/products'
-    );
+    return this._HttpClient.get(this.baseUrl + "api/v1/products");
   }
 
   getProductsDetails(id: string): Observable<any> {
-    return this._HttpClient.get(
-      `https://route-ecommerce.onrender.com/api/v1/products/${id}`
-    );
+    return this._HttpClient.get(this.baseUrl + `api/v1/products/${id}`);
   }
 
   getCategories(): Observable<any> {
-    return this._HttpClient.get(
-      'https://route-ecommerce.onrender.com/api/v1/categories'
-    );
+    return this._HttpClient.get(this.baseUrl + "api/v1/categories");
   }
 }
--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,46 +1,39 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import jwtDecode from 'jwt-decode';
-import { BehaviorSubject, Observable } from 'rxjs';
-
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import jwtDecode from "jwt-decode";
+import { BehaviorSubject, Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class RegisterService {
+  baseUrl: string = "https://ecommerce.routemisr.com/";
   userInfo = new BehaviorSubject(null);
 
-  constructor(private _HttpClient: HttpClient,
-    private _Router: Router) {
-    if (localStorage.getItem('userToken') !== null) {
-      this.decodeUserInfo()
+  constructor(private _HttpClient: HttpClient, private _Router: Router) {
+    if (localStorage.getItem("userToken") !== null) {
+      this.decodeUserInfo();
     }
   }
 
   decodeUserInfo() {
-    let encodedToken = JSON.stringify(localStorage.getItem('userToken'));
+    let encodedToken = JSON.stringify(localStorage.getItem("userToken"));
     let decodedToken: any = jwtDecode(encodedToken);
     this.userInfo.next(decodedToken);
   }
 
   logOut() {
-    localStorage.removeItem('userToken');
+    localStorage.removeItem("userToken");
     this.userInfo.next(null);
-    this._Router.navigate(['/login']);
+    this._Router.navigate(["/login"]);
   }
 
   register(userData: object): Observable<any> {
-    return this._HttpClient.post(
-      'https://route-ecommerce.onrender.com/api/v1/auth/signup',
-      userData
-    );
+    return this._HttpClient.post(this.baseUrl + "api/v1/auth/signup", userData);
   }
 
   login(userData: object): Observable<any> {
-    return this._HttpClient.post(
-      "https://route-ecommerce.onrender.com/api/v1/auth/signin",
-      userData
-    );
+    return this._HttpClient.post(this.baseUrl + "api/v1/auth/signin", userData);
   }
 }
