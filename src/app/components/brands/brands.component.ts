27f09b7759762fb5c2f98298438e3f44416160f6
mythic357 <101456209+mythic357@users.noreply.github.com>
2023-05-21 14:20:05 +0300

    brands

--- src/app/components/brands/brands.component.ts
+++ src/app/components/brands/brands.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Brand } from './../../interfaces/product';
+import { Component, OnInit } from '@angular/core';
+import { BrandsService } from 'src/app/services/brands.service';
+
+
 
 @Component({
   selector: 'app-brands',
   templateUrl: './brands.component.html',
   styleUrls: ['./brands.component.css']
 })
-export class BrandsComponent {
+export class BrandsComponent implements OnInit {
+  brands:Brand[] = [];
 
+  constructor(private _BrandsService:BrandsService){}
+
+  getAllBrands(){
+    this._BrandsService.brands().subscribe({
+      next:(res)=>{
+        console.log(res.data);
+        this.brands = res.data
+      }
+    })
+  }
+
+
+  ngOnInit(): void {
+      this.getAllBrands()
+  }
 }
